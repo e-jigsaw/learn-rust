ccf6e9e3f2ddef8b5ac31c4d9893786aaa3247d0
jigsaw <jigsaw@live.jp>
2015-10-20 19:53:51 +0900

    It works test

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -1,5 +1,8 @@
+pub fn add_two(a: i32) -> i32 {
+    a + 2
+}
+
 #[test]
-#[should_panic(expected = "assertion failed")]
 fn it_works() {
-    assert_eq!("Hello", "world");
+    assert_eq!(4, add_two(2));
 }
